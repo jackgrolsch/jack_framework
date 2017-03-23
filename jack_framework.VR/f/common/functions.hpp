b4bf2989b7c17316308e9709322bd454e38b9dac
jackgrolsch <mahe@viaccess.fr>
2017-03-23 11:23:27 +0100

    Purging inithial code
    Removal of clutter and non used features from the F3

--- jack_framework.VR/f/common/functions.hpp
+++ jack_framework.VR/f/common/functions.hpp
@@ -14,74 +14,20 @@ class F // Defines the "owner"
 		class mpEnd{};
 		class mpEndReceiver{};
 	};
-	class assignGear
-	{
-		file = "f\assignGear";
-		class assignGear{};
-	};
 	class setGroupID
 	{
 		file = "f\setGroupID";
 		class setGroupID{};
-	};
-	class missionConditions
-	{
-		file = "f\missionConditions";
-		class SetTime{};
-		class SetFog{};
-		class SetWeather{};
-		class SetWind{};
-	};
-	class cache
-	{
-		file = "f\cache";
-		class cInit {};
-		class cTracker {};
-		class gCache {};
-		class gUncache {};
 	};
 	class groupMarkers {
 		file = "f\groupMarkers";
 		class localGroupMarker{};
 		class localSpecialistMarker{};
 	};
-	class authorisedCrew
-	{
-		file = "f\authorisedCrew";
-		class authorisedCrewCheck {};
-	};
-	class FTMemberMarkers
-	{
-		file = "f\FTMemberMarkers";
-		class SetLocalFTMemberMarkers{};
-		class GetMarkerColor{};
-		class LocalFTMarkerSync{};
-	};
 	class bodyRemoval
 	{
 		file = "f\removeBody";
 		class removeBody{};
-	};
-	class setAISkill
-	{
-		file = "f\setAISKill";
-		class setAISKill{};
-	};
-	class mapClickTeleport
-	{
-		file = "f\mapClickTeleport";
-		class mapClickTeleportUnit{};
-		class mapClickTeleportGroup{};
-	};
-	class nametag
-	{
-		file = "f\nametag";
-		class drawNameTag{};
-	};
-	class preMount
-	{
-		file = "f\preMount";
-		class mountGroups{};
 	};
 	class zeus
 	{
@@ -102,21 +48,6 @@ class F // Defines the "owner"
 	{
 		file = "f\safeStart";
 		class safety{};
-	};
-	class woundingsystem
-	{
-		file = "f\simplewoundingsystem";
-		class SetDowned {};
-		class WoundedEffect{};
-		class HasWounded{};
-		class EjectWounded{};
-		class OnDrag{};
-		class LifeTick {};
-		class OnDeath {};
-		class OnHeal {};
-		class OnDamage{};
-		class SetBleeding{};
-		class GetAnimation{};
 	};
 	class fspectator
 	{
